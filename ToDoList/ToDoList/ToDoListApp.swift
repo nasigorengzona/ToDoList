d3fa149481ff04b7117c05de11f73f5a611d4d60
Erlina Ng <51814748+nasigorengzona@users.noreply.github.com>
2022-10-25 07:22:52 +0700

    Add files via upload

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by erlina ng on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
